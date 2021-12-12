45dea6e9d057a086fde744d70198c35e469b78d3
Bjorn Stange <bjorn248@gmail.com>
2021-12-12 00:10:46 -0500

    use paxcalculator domain

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scca-pax-calculator
 This automatically generates a static website to use as a PAX Calculator for SCCA Solo (Autocross) competition.
+
+Uses [https://www.solotime.info/pax/](https://www.solotime.info/pax/) as the source of truth.
+
+Hosted at [https://www.paxcalculator.com](https://www.paxcalculator.com)
+
+## Build Status
+[![Build Status](https://github.com/Bjorn248/scca-pax-calculator/actions/workflows/build.js.yml/badge.svg)](https://github.com/Bjorn248/scca-pax-calculator/actions/workflows/build.js.yml)
--- terraform/infra.tf
+++ terraform/infra.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.50"
+      version = "~> 3"
     }
   }
 
   backend "s3" {
     bucket = "bjornaws-terraform"
-    key    = "pax-calculator"
+    key    = "paxcalculator"
     region = "us-east-1"
   }
 }
@@ -21,12 +21,12 @@ provider "aws" {
 module "s3-static-site" {
   source = "github.com/Bjorn248/s3-static-site"
 
-  root-domain            = "pax-calculator.com"
-  target-domain          = "www.pax-calculator.com"
+  root-domain            = "paxcalculator.com"
+  target-domain          = "www.paxcalculator.com"
   cloudfront-price-class = "PriceClass_100"
 
   global-tags = {
-    project = "pax-calculator"
+    project = "paxcalculator"
   }
 }
 
@@ -36,6 +36,6 @@ module "s3-website-deployment-iam" {
   s3-bucket-name = module.s3-static-site.s3_bucket_name
 
   global-tags = {
-    project = "pax-calculator"
+    project = "paxcalculator"
   }
 }
