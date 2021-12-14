5687d724f8c506601f2181b9611f6e00a5c2cf01
Bjorn Stange <bjorn248@gmail.com>
2021-12-13 20:49:37 -0500

    first stage script version

---
+++ stage_changes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Building site generator..."
+go build main.go
+echo "Done"
+
+echo "Running site generator..."
+./main
+
+branch_name="generated-changes"
+
+if ! git diff-index --quiet HEAD --
+then
+  echo "Found a difference in git!"
+  git stash
+  echo "Checking for existence of $branch_name branch..."
+  if git rev-parse --verify $branch_name
+  then
+    echo "Found branch, deleting"
+    git branch -D $branch_name
+  fi
+  git checkout -b $branch_name
+  git stash pop
+  git add -A
+  git commit -m 'auto-generated changes'
+  git push origin $branch_name
+else
+  echo "Found no difference in the generated file, exiting"
+  exit 0
+fi
